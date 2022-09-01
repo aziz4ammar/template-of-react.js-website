366d5f2df5dfeea2247ebde4dbecdf1a5e608f06
mohamed aziz ammar <azizammar67@gmail.com>
2022-09-01 21:45:40 +0100

    fixing items

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,11 +1,11 @@
 export const tab = [
-    {id:Math.random() , image:"https://auspost.com.au/shop/static/WFS/AusPost-Shop-Site/-/AusPost-Shop/en_AU/product/059069724/1/org.png",
-name:"Toronto",Price:10,cont:1,
+    {id:Math.random() , image:"https://c0.wallpaperflare.com/preview/783/732/25/architecture-buildings-canada-city.jpg",
+name:"Toronto",text:"City in Canada",
 },
-{id:Math.random() , image:"https://auspost.com.au/shop/static/WFS/AusPost-Shop-Site/-/AusPost-Shop/en_AU/product/059069724/1/org.png",
-name:"Ottawa",Price:10,cont:1,
+{id:Math.random() , image:"https://a.cdn-hotels.com/gdcs/production163/d857/cc29dd0e-f745-4507-80e1-6ae5a3532338.jpg",
+name:"Ottawa",text:"Capital of Canada",
 },
-{id:Math.random() , image:"https://auspost.com.au/shop/static/WFS/AusPost-Shop-Site/-/AusPost-Shop/en_AU/product/059069724/1/org.png",
-name:"British Colombia",Price:10,cont:1,
+{id:Math.random() , image:"https://wallpaperaccess.com/full/642001.jpg",
+name:"British Colombia",text:"Canadian Province",
 },
 ]
--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -7,13 +7,10 @@ const Body = ({el}) => {
     <div>
       <div>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={el.image} />
       <Card.Body>
         <Card.Title>{el.name}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Text>{el.text}</Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
       </Card>
