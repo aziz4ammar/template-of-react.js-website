2d86f1c5a307ccb94dafa1a3a42001fb3de256b5
mohamed aziz ammar <azizammar67@gmail.com>
2022-09-02 23:13:33 +0100

    edit

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,11 +1,11 @@
 export const tab = [
     {id:Math.random() , image:"https://c0.wallpaperflare.com/preview/783/732/25/architecture-buildings-canada-city.jpg",
-name:"Toronto",text:"City in Canada",
+    name:"Toronto",text:"City in Canada",
+},
+{id:Math.random() , image:"https://media.routard.com/image/31/8/vancouver-2.1488318.142.jpg",
+name:"Vancouver",text:"City in Canada",
 },
 {id:Math.random() , image:"https://a.cdn-hotels.com/gdcs/production163/d857/cc29dd0e-f745-4507-80e1-6ae5a3532338.jpg",
 name:"Ottawa",text:"Capital of Canada",
-},
-{id:Math.random() , image:"https://wallpaperaccess.com/full/642001.jpg",
-name:"British Colombia",text:"Canadian Province",
 },
 ]
