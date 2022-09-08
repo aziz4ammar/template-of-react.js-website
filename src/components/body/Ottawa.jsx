4b558a6941606d4d58374f85fd8636317c8198de
mohamed aziz ammar <azizammar67@gmail.com>
2022-09-08 22:12:38 +0100

    commit number 15

---
+++ src/components/body/Ottawa.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbaro from '../head/Navbaro'
+
+const Ottawa = () => {
+  return (
+    <div>
+        <Navbaro/>
+    </div>
+  )
+}
+
+export default Ottawa
---
+++ src/components/body/Vancouver.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbaro from '../head/Navbaro'
+
+const Vancouver = () => {
+  return (
+    <div>
+        <Navbaro/>
+    </div>
+  )
+}
+
+export default Vancouver
