5a437db08b715b032ecc2901203190e1e96830fb
mohamed aziz ammar <azizammar67@gmail.com>
2022-09-10 21:06:25 +0100

    commit number 18

--- src/components/body/Ottawa.jsx
+++ src/components/body/Ottawa.jsx
@@ -1,12 +1,89 @@
-import React from 'react'
-import Navbaro from '../head/Navbaro'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
-const Ottawa = () => {
+const Edit = ({ el, edit }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [name, setName] = useState(el.name);
+  const [image, setImage] = useState(el.image);
+  const [rating, setRating] = useState(el.rating);
+  const [date, setdate] = useState(el.date);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const editedMovie = {
+      id: el.id,
+      name,
+      image,
+      rate,
+      date,
+    };
+    edit(editedMovie);
+    handleClose();
+  };
   return (
     <div>
-        <Navbaro/>
-    </div>
-  )
-}
+      <Button variant="primary" onClick={handleShow}>
+        Edit
+      </Button>
 
-export default Ottawa
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Movie</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Title</InputGroup.Text>
+              <Form.Control
+                aria-label="First name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Image</InputGroup.Text>
+              <Form.Control
+                aria-label="First name"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Rating</InputGroup.Text>
+              <Form.Control
+                aria-label="First name"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>Year</InputGroup.Text>
+              <Form.Control
+                aria-label="First name"
+                value={date}
+                onChange={(e) => setdate(e.target.value)}
+              />
+            </InputGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </div>
+  );
+};
+
+export default Edit;
